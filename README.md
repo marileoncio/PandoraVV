28e639401936842e28d3455c2a629ebf8ea5a543
marileoncio <mariana.timoteo19@yahoo.com>
2024-01-30 09:09:48 -0300

    commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 <h1 align="center"> Pandora Vogue Velvet </h1>
-<img src="Pandora VV.jpg" alt="Texto Alternativo">
+<img src="Pandora VV.jpg" alt="LOGO" align="center" >
--- app/Models/ADM.php
+++ app/Models/ADM.php
@@ -10,18 +10,8 @@ class ADM extends Model
     use HasFactory;
     protected $fillable=[
         'nome',
-        'celular',
         'email',
         'cpf',
-        'dataNascimento',
-        'cidade',
-        'estado',
-        'pais',
-        'rua',
-        'numero',
-        'bairro',
-        'cep',
-        'complemento',
         'senha'
 ];
 }
